a4c4fc4d98b5fcf94355cd03c6808076fcc82c4f
correak <karendelpilar.correa@gmail.com>
2026-01-07 14:25:39 -0500

    actualizando api

--- app.py
+++ app.py
@@ -9,15 +9,16 @@ import os
 app = FastAPI()
 
 origins = [
-    "http://localhost:5500",
-    "http://localhost",
+    "http://localhost:5500",   # 
     "http://127.0.0.1:5500",
-   ## "https://capy-ai-api.onrender.com"
+    "http://localhost",
+    "http://127.0.0.1",
+    "https://capy-ai-api.onrender.com" 
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
